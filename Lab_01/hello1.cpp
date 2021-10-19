fd701193d9596afcf6ea482a66d908e1c10208ae
Soraya <60835721+sorayaboza@users.noreply.github.com>
2021-10-18 17:39:59 -0700

    Delete Lab_01 directory

--- Lab_01/hello1.cpp
+++
@@ -1,9 +0,0 @@
-// Basic "HelloWorld" program.
-// Ctrl+B to show/hide the Explorer Tab.
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<"Hello World!\n"; // \n makes a new line.
-}
-
